f7a9427436de66d29847457079b93e7ced9583fd
Eli Holmes <eli.holmes@noaa.gov>
2022-02-15 03:05:13 +0000

    first commit

---
+++ R/data-cars.R
@@ -0,0 +1,18 @@
+#' @title a dataset of horsepower for different cars
+#'
+#' @description First 4 columns of the mtcars dataset.
+#'
+#' \itemize{
+#'   \item mpg. miles per gallon
+#'   \item cyl. cylinders
+#'   \item disp. displacement
+#'   \item hp. horse poser
+#' }
+#'
+#' @docType data
+#' @name cars
+#' @usage data(cars)
+#' @references R base package.
+#' @format A data frame.
+#' @keywords datasets
+NULL
---
+++ R/data-iris2.R
@@ -0,0 +1,16 @@
+#' @title sepal length for differen species of iris
+#'
+#' @description The Species and Sepal.Length from the iris data set from base R.
+#'
+#' \itemize{
+#'   \item Species. species
+#'   \item Sepal.Length. sepal length
+#' }
+#'
+#' @docType data
+#' @name iris2
+#' @usage data(iris2)
+#' @references from R base package.
+#' @format A data frame.
+#' @keywords datasets
+NULL
